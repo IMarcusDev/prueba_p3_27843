79e752406855e9f8405207b0cff82916822ff40f
MatSosa1 <dmsosa3@espe.edu.ec>
2026-02-03 08:33:28 -0500

    Better ViajeView Test

--- test/Ej3/viaje_view_test.dart
+++ test/Ej3/viaje_view_test.dart
@@ -5,12 +5,12 @@ import 'package:prueba_app/view/viaje_view.dart';
 
 void main() {
   testWidgets('Prueba de Página Viaje', (WidgetTester tester) async {
-    await tester.pumpWidget(PaginaView());
+    await tester.pumpWidget(MaterialApp(home: PaginaView(),));
 
     final field = find.byType(TextField);
     await tester.enterText(field, '100000000000');
-    tester.tap(find.byType(ElevatedButton));
-    await tester.pump();
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
 
     expect(find.text("El número es demasiado grande, intente otro número mas pequeño"), findsOneWidget);
   });
